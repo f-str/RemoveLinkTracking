860bbf4f938c9a5e53fd31d49922231ae7bd4edb
Fload <fload2000@protonmail.com>
2020-04-16 18:14:36 +0200

    URL-Exceptions are considered by the background script

--- src/background.js
+++ src/background.js
@@ -1,22 +1,46 @@
-function removeTracking({ url }) {
-  if (!REMOVER.mayContain(url)) {
-    return
-  }
-
-  const redirectUrl = REMOVER.remove(url);
-
-  if (redirectUrl === url) {
-    return
-  }
-
-  return {
-    redirectUrl
-  }
-}
-
 const browser = window.browser || window.chrome;
 browser.webRequest.onBeforeRequest.addListener(
-  removeTracking,
-  { urls: ['<all_urls>'], types: ['main_frame', 'sub_frame'] },
-  ['blocking']
+    removeTracking,
+    {urls: ['<all_urls>'], types: ['main_frame', 'sub_frame']},
+    ['blocking']
 );
+
+// Set the default empty list on installation.
+browser.runtime.onInstalled.addListener(() => {
+    browser.storage.local.set({
+        exceptions: []
+    });
+});
+
+let exceptions = [];
+
+// Get the stored list of exceptions
+browser.storage.local.get(data => {
+    if (data.exceptions) {
+        exceptions = data.exceptions;
+    }
+});
+
+// Listen for changes in the exceptions list
+browser.storage.onChanged.addListener(changeData => {
+    exceptions = changeData.exceptions.newValue;
+});
+
+function removeTracking({url}) {
+
+    if (exceptions.indexOf((new URL(url).hostname)) !== -1) {
+        return
+    } else if (!REMOVER.mayContain(url)) {
+        return
+    }
+
+    const redirectUrl = REMOVER.remove(url);
+
+    if (redirectUrl === url) {
+        return
+    }
+
+    return {
+        redirectUrl
+    }
+}
