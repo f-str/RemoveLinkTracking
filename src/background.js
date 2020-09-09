8a6967cc9abc0d6ed6a6a0d0451a1db81c1f9778
Fload <fload2000@protonmail.com>
2020-09-09 16:29:02 +0200

    Read all stored fields on install or upgrade.

--- src/background.js
+++ src/background.js
@@ -6,19 +6,19 @@ browser.webRequest.onBeforeRequest.addListener(
     ['blocking']
 );
 
-// Generate map from keywords and true
-const paramMap = new Map();
-for(let keyword of Keywords) {
-    paramMap.set(keyword, true)
-}
-
 // Set the default empty list on installation.
 browser.runtime.onInstalled.addListener((details) => {
     if (details.reason === 'install' || details.reason === 'update') {
         let active = Keywords;
+        let paramMap = new Map();
         let deletedParams = deleted_Keywords;
         let logs = [];
+        let exceptions = [];
+        let ownParam = false;
+        let logging = true;
+        let showPageAction = false;
 
+        // Get currently stored values
         browser.storage.local.get(data => {
             if (data.deletedParams) {
                 deletedParams = deletedParams.concat(data.deletedParams).unique();
@@ -26,19 +26,45 @@ browser.runtime.onInstalled.addListener((details) => {
             if (data.active) {
                 active = active.concat(data.active).unique().filter((el) => !deletedParams.includes(el));
             }
+            if (data.parameters) {
+                paramMap = new Map(JSON.parse(data.parameters))
+            }
             if (data.logs) {
                 logs = data.logs;
             }
+            if (data.exceptions) {
+                exceptions = data.exceptions;
+            }
+            if (data.logging) {
+                logging = data.logging.valueOf();
+            }
+            if (data.ownParam) {
+                ownParam = data.ownParam.valueOf();
+            }
+            if (data.showPageAction) {
+                showPageAction = data.showPageAction.valueOf();
+            }
         });
 
+        // Add new Keywords to parameterMap
+        for(let keyword of Keywords) {
+            if (!paramMap.has(keyword)) {
+                paramMap.set(keyword, true);
+            }
+        }
+
+        // Remove deleted keywords from parameter map
+        deleted_Keywords.forEach(key => paramMap.delete(key))
+
+        // Store new values
         browser.storage.local.set({
-            exceptions: [],
+            exceptions: exceptions,
             parameters: JSON.stringify(Array.from(paramMap.entries())),
             active: active,
-            ownParam: false,
-            logging: true,
+            ownParam: ownParam,
+            logging: logging,
             logs: logs,
-            showPageAction: false,
+            showPageAction: showPageAction,
             deletedParams: deletedParams
         });
     }
